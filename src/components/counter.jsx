7ace281d3e0da8e945169203a775c5617e495222
Evan <oakeef@gmail.com>
2022-02-06 21:57:36 -0400

    add some html tags to the jsx, create some functions, add things to state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+    state = {
+        count: 0,
+        imageUrl: "https://picsum.photos/200",
+        tags: ["tag1", "tag2", "tag3"],
+    };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold",
+    };
+
     render() {
         return (
             <React.Fragment>
-                <h1>Hello World</h1>
-                <button>Increment</button>
+                <img src={this.state.imageUrl} alt=""></img>
+                <span style={this.styles} className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map((tag) => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 
