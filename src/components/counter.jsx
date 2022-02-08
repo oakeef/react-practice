115fe3a062899f339f38ec925c121e2de3af324a
Evan <oakeef@gmail.com>
2022-02-08 11:06:40 -0400

    modify counter to take prop value, modify COunters to pass prop value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
     };
 
-    handleIncrement = (product) => {
-        this.setState({ count: this.state.count + 1 });
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 });
     };
 
     render() {
@@ -16,9 +16,7 @@ class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button
-                    onClick={() => {
-                        this.handleIncrement({id:1});
-                    }}
+                    onClick={this.handleIncrement}
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
@@ -29,13 +27,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 bg-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
 
