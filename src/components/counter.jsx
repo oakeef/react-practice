0cba7c197fb6231a238ce8d5dd5e74162e731653
Evan <oakeef@gmail.com>
2022-02-08 10:47:12 -0400

    remove un-needed img, list etc. create function to incrememnt count on button press

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,25 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: "https://picsum.photos/200",
-        tags: ["tag1", "tag2", "tag3"],
     };
 
-    styles = {
-        fontSize: 20,
-        fontWeight: "bold",
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
     };
 
     render() {
         return (
-            <React.Fragment>
-                <img src={this.state.imageUrl} alt=""></img>
+            <div>
                 <span style={this.styles} className={this.getBadgeClasses()}>
                     {this.formatCount()}
                 </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map((tag) => (
-                        <li key={tag}>{tag}</li>
-                    ))}
-                </ul>
-            </React.Fragment>
+                <button
+                    onClick={this.handleIncrement}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+            </div>
         );
     }
 
